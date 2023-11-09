b0d98f19deb46d8b3d81881bc9a839fd93754de2
tientt37 <trantrungtien9x.gmail.com>
2023-11-09 16:21:12 +0700

    Fetch data

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -1,26 +1,44 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import {FindStockRequest, ObjectRecords, StockHistory, TemplateResponse} from './models/StockHistory.model';
+import {
+  FindStockRequest,
+  ObjectRecords,
+  StockHistory,
+  TemplateResponse,
+} from './models/StockHistory.model';
 import { Observable } from 'rxjs';
 
 const headers = new HttpHeaders();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StockService {
   apiUrl = 'http://localhost:8080/api/v1/stock/find-hot-stock';
-  constructor(private httpClient:HttpClient) { }
+  dummyProject = 'https://dummyjson.com/products';
 
+  constructor(private httpClient: HttpClient) {}
 
   public get(options?: any) {
-    return this.httpClient.get("http://localhost:8080/api/v1/test/hi").subscribe((response) => { console.log(response); },
-    (error) => { console.log(error); });
-    }
+    return this.httpClient
+      .get('http://localhost:8080/api/v1/test/hi')
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 
-  findStock(request:FindStockRequest):Observable<any>{
+  findStock(request: FindStockRequest): Observable<any> {
     return this.httpClient.post(this.apiUrl, request);
+  }
+
+  findProject() {
+    return this.httpClient.get(this.dummyProject);
   }
 }
